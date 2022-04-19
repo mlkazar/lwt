c350a70a9a09e3dd31e7ac7f59033bb6e9452bb4
Thomas Hicks <thhicks@microsoft.com>
2022-04-19 14:11:30 -0400

    Change osp new test to use a smaller large object size to avoid being reaped by a linux kernel. Also test the first and last element of memory being allocated to ensure memory is mapped as expected.

--- tests/test_ospnew.cc
+++ tests/test_ospnew.cc
@@ -1,25 +1,41 @@
 #include <gtest/gtest.h>
 #include "ospnew.h"
 
+//256M entries
+#define LARGE_MEM_ENTRIES (268435456ULL)
+
 TEST(OspTestNew,Basic)
 {
     struct Foo1  { int data[1]; };
 
     struct Foo32 { int data[32]; };
 
-    struct Foo4G { int data[/* 4*1024*1024*1024*/ 4294967296]; };
+    struct Foo1G { int data[LARGE_MEM_ENTRIES]; };
 
     auto *pFoo1 = new Foo1();
     auto *pFoo32 = new Foo32();
-    auto *pFoo4G = new Foo4G();
-    EXPECT_TRUE(pFoo1!=NULL);
-    EXPECT_TRUE(pFoo32!=NULL);
-    EXPECT_TRUE(pFoo4G!=NULL);
+    auto *pFoo1G = new Foo1G();
 
+    pFoo1->data[0] = 0;
+    pFoo32->data[31] = 31;
+    pFoo32->data[0] = 32;
+    pFoo1G->data[LARGE_MEM_ENTRIES-1] = LARGE_MEM_ENTRIES-1;
+    pFoo1G->data[0] = LARGE_MEM_ENTRIES;
+    
+    EXPECT_TRUE(pFoo1!=NULL);
+    EXPECT_EQ(pFoo1->data[0],0);
+
+    EXPECT_TRUE(pFoo32!=NULL);
+    EXPECT_EQ(pFoo32->data[0],32);
+    EXPECT_EQ(pFoo32->data[31],31);
+    
+    EXPECT_TRUE(pFoo1G!=NULL);
+    EXPECT_EQ(pFoo1G->data[0],LARGE_MEM_ENTRIES);
+    EXPECT_EQ(pFoo1G->data[LARGE_MEM_ENTRIES-1],LARGE_MEM_ENTRIES-1);
+    
     delete pFoo1;
     delete pFoo32;
-    delete pFoo4G;
-
+    delete pFoo1G;
 }
 
 TEST(OspTestNew,DeleteNull)
